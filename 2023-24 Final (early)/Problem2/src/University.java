f87d88eddbf1ec50cb7e56f62d25397aa4e000bb
Alonso Herreros <alonso.herreros.c@gmail.com>
2024-01-14 16:00:27 +0100

    Solve F.23-24early.P2.1

--- 2023-24 Final (early)/Problem2/src/University.java
+++ 2023-24 Final (early)/Problem2/src/University.java
@@ -1,20 +1,22 @@
+import java.util.concurrent.*;
+
 public class University {
     public static void main(String[] args) {
         Student st;
         Semaphore rASem, rBSem, rCSem;
-        ______________ mutexA, mutexB, mutexC;
+        Semaphore mutexA, mutexB, mutexC;
 
-        rASem = new Semaphore(__, true); //A Cafeteria
-        rBSem = new Semaphore(__, true); //B Canteen
-        rCSem = new Semaphore(__, true); //C Class
+        rASem = new Semaphore(40, true); //A Cafeteria
+        rBSem = new Semaphore(30, true); //B Canteen
+        rCSem = new Semaphore(20, true); //C Class
 
-        mutexA = new Semaphore(__);
-        mutexB = new Semaphore(__);
-        mutexC = new Semaphore(__);
+        mutexA = new Semaphore(1);
+        mutexB = new Semaphore(1);
+        mutexC = new Semaphore(1);
 
-        for (int i = 0; i < __; i++) {
-            st = new _________(i, rASem, rBSem, rCSem, mutexA, mutexB, mutexC);
-            st._________();
+        for (int i = 0; i < 100; i++) {
+            st = new Student(i, rASem, rBSem, rCSem, mutexA, mutexB, mutexC);
+            st.start();
         }
     }
 }
